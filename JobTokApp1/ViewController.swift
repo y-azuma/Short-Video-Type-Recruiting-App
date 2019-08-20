f47f387ef206d7cea9e56e23be2568d3a981d179
Yudai Azuma <y.azuma@kyotoinnovation.jp>
2019-08-20 15:43:35 +0900

    Initial Commit

---
+++ JobTokApp1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  JobTokApp1
+//
+//  Created by 東　雄大 on 2019/08/20.
+//  Copyright © 2019 Azuma Yudai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ JobTokApp1Tests/JobTokApp1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  JobTokApp1Tests.swift
+//  JobTokApp1Tests
+//
+//  Created by 東　雄大 on 2019/08/20.
+//  Copyright © 2019 Azuma Yudai. All rights reserved.
+//
+
+import XCTest
+@testable import JobTokApp1
+
+class JobTokApp1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
